14952b4865ced930d93632d6b6873d63ec71747c
aadinath16 <adi.dhamdhere@yahoo.com>
2025-06-26 19:18:11 +0000

    fourth commit

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/hello1')
     assert response.status_code == 200
-    assert b"Hello from Flask-1" in response.data
+    assert b"Hello from Flask" in response.data
