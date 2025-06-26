9a4f1adfb0eaf7270dd69d4e11354058bc259ba6
aadinath16 <adi.dhamdhere@yahoo.com>
2025-06-26 19:39:05 +0000

    sixth commit

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get('/hello1')
     assert response.status_code == 200  # nosec B101
-    assert b"Hello from Flask" in response.data
+    assert b"Hello from Flask" in response.data # nosec B101
