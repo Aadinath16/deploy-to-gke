bd0b0d41db27a0d1e1cde49eaac0193a769a8360
aadinath16 <adi.dhamdhere@yahoo.com>
2025-06-26 19:35:14 +0000

    fifth commit

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello():
     return "Hello from Flask-1 on GKE!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)  # nosec B104
--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get('/hello1')
-    assert response.status_code == 200
+    assert response.status_code == 200  # nosec B101
     assert b"Hello from Flask" in response.data
